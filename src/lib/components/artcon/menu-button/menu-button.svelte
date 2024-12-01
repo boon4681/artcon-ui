597ba82e857516dfa996fdf5a2d2668af2f97fa8
boon4681 <boon4681dev@gmail.com>
2024-12-01 14:33:13 +0700

    add: menu-button

---
+++ src/lib/components/artcon/menu-button/menu-button.svelte
@@ -0,0 +1,1 @@
+<div class="bg-white hover:bg-neutral-200 transition-all rounded p-1 text-sm px-2 cursor-pointer">My Profile</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import MenuButton from "$lib/components/artcon/menu-button/menu-button.svelte";
+</script>
+
+<div class="flex">
+    <MenuButton></MenuButton>
+</div>
