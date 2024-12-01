7504ff038dec0c32d831190a82e39aa8e9f70b2c
PhonePlaza <phonekungkub@gmail.com>
2024-12-01 15:25:15 +0700

    add heart-button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
     import MenuButton from "$lib/components/artcon/menu-button/menu-button.svelte";
+    import HeartButton from "$lib/components/artcon/heart-button/heart-button.svelte";
 </script>
 
 <div class="flex">
     <MenuButton></MenuButton>
+    <HeartButton></HeartButton>
 </div>
