6982da73d7ae8286657b6393e6e7dd5f38befdf7
PhonePlaza <phonekungkub@gmail.com>
2024-12-04 17:28:07 +0700

    heart

--- src/lib/components/artcon/heart-button/heart-button.svelte
+++ src/lib/components/artcon/heart-button/heart-button.svelte
@@ -8,6 +8,7 @@
 <div on:click={toggleheart} class="cursor-pointer inline-block">
     <HeartIcon
         class={`w-${size} h-${size} ${isClicked ? "text-red-500" : "text-black-400"}`}
+        
         fill={isClicked ? 'red' : 'none'}
         stroke="currentColor"
     />
