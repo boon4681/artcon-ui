60da705dcf2bb5004e5345e0cc510018b37ed17f
wanittra <wanittra.fern@gmail.com>
2024-12-04 21:51:48 +0700

    86cx71rgr

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
-    import MenuButton from "$lib/components/artcon/menu-button/menu-button.svelte";
+    import MenuButton from "$lib/components/artcon/menu-button/menu-button.svelte";   
 </script>
+
 
 <div class="flex">
     <MenuButton></MenuButton>
