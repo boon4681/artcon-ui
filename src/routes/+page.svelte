8be912e7939847ca5bd64a0ed628c89597d07be6
wanittra <wanittra.fern@gmail.com>
2024-12-05 20:59:22 +0700

    add:ButtonIcon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script>
     import MenuButton from "$lib/components/artcon/menu-button/menu-button.svelte";   
+    //@ts-ignore
+    import { ArrowBigUpDash } from 'lucide-svelte';
 </script>
 
 
 <div class="flex">
     <MenuButton></MenuButton>
+    <ArrowBigUpDash class='hover:border'/>
 </div>
