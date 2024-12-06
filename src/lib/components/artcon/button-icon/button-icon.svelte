ed29ae989fcf1062036e1d60c96364addd39d323
wanittra <wanittra.fern@gmail.com>
2024-12-06 19:44:32 +0700

    update:buttonIcon

--- src/lib/components/artcon/button-icon/button-icon.svelte
+++ src/lib/components/artcon/button-icon/button-icon.svelte
@@ -1,2 +1,9 @@
+<script>
+    //@ts-ignore
+    import { Share } from 'lucide-svelte';
+</script>
 
+<div class="flex">
+    <Share  class='hover:bg-[#E9E9E9] w-[44px] h-[44px] p-2 rounded-lg cursor-pointer'/>
+</div>
     
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,10 @@
 <script>
     import MenuButton from "$lib/components/artcon/menu-button/menu-button.svelte";   
-    //@ts-ignore
-    import { Share } from 'lucide-svelte';
+    import ButtonIcon from "$lib/components/artcon/button-icon/button-icon.svelte";
 </script>
 
 
 <div class="flex">
     <MenuButton></MenuButton>
-    <Share  class='hover:bg-[#E9E9E9] w-[44px] h-[44px] p-2 rounded-lg'/>
+    <ButtonIcon></ButtonIcon>
 </div>
