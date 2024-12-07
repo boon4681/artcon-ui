93e9f7d0610f48c0fbd2919feab94dee7353f98b
wanittra <wanittra.fern@gmail.com>
2024-12-07 21:43:54 +0700

    add-update:banner

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
     import MenuButton from "$lib/components/artcon/menu-button/menu-button.svelte";
+    import Banner from "$lib/components/artcon/banner/banner.svelte";
 </script>
 
 <div class="flex">
     <MenuButton></MenuButton>
+    <Banner></Banner>
 </div>
