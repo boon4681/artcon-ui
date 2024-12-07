d3ec39b62b83b800d8f4a8a4f48e8e1b2f81369d
PhonePlaza <phonekungkub@gmail.com>
2024-12-07 10:14:26 +0700

    add squarecard

---
+++ src/lib/components/artcon/heart-button/heart-button.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { Heart as HeartIcon } from "lucide-svelte";
+    export let size = 36;
+    let isClicked = false;
+    const toggleheart = () => (isClicked = !isClicked);
+</script>
+
+<div on:click={toggleheart} class="cursor-pointer inline-block">
+    <HeartIcon
+        class={`w-${size} h-${size} ${isClicked ? "text-red-500" : "text-black-400"}`}
+        fill={isClicked ? 'red' : 'none'}
+        stroke="currentColor"
+    />
+</div>
---
+++ src/lib/components/artcon/squarecard/squarecard.svelte
@@ -0,0 +1,30 @@
+<script>
+    import HeartButton from "../heart-button/heart-button.svelte";
+    export let src;
+    export let alt;
+
+    let isClicked = false;
+
+    function handleClick() {
+        isClicked = true;
+        setTimeout(() => {
+            isClicked = false;
+        }, 100); 
+    }
+</script>
+
+<div class="relative w-40 h-40 rounded-lg overflow-hidden shadow-lg">
+    <div class="group" on:click={handleClick}>
+        <img {src} {alt} class="object-cover transition duration-300" />
+
+        <div
+            class="absolute inset-0 bg-black transition duration-300 opacity-0"
+            class:opacity-80={isClicked}
+            class:group-hover:opacity-20={!isClicked}
+        ></div>
+    </div>
+
+    <div class="absolute bottom-2 right-2 hover:scale-110">
+        <HeartButton />
+    </div>
+</div>
