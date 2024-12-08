fa1c68446cdab2cfa2a92d6787f61b07df7f6c3c
Papaerungrat <papaerungrat@gmail.com>
2024-12-08 11:00:47 +0700

    add follow-button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,15 @@
 <script>
     import MenuButton from "$lib/components/artcon/menu-button/menu-button.svelte";
+    import FollowButton from "$lib/components/follow-button/follow-button.svelte";
 </script>
 
 <div class="flex">
     <MenuButton></MenuButton>
+    <script>
+        import FollowButton from "./components/FollowButton.svelte";
+    </script>
+
+    <main>
+        <FollowButton />
+    </main>
 </div>
