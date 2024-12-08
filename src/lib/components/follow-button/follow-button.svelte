dfd1ed4eebeffff6a56d12213036172b5d3e956a
boon4681 <boon4681dev@gmail.com>
2024-12-08 14:56:47 +0700

    fix: follow button size

--- src/lib/components/follow-button/follow-button.svelte
+++ src/lib/components/follow-button/follow-button.svelte
@@ -8,11 +8,11 @@
 
 <button
     on:click={toggleFollow}
-    class="flex items-center justify-center px-6 py-2 border-2 rounded-full text-white transition-all duration-300"
+    class="flex w-32 items-center justify-center px-6 py-2 border-2 rounded-full text-white transition-all duration-300"
     class:bg-gray-300={isFollowing}
-    class:bg-blue-400={!isFollowing}
+    class:bg-blue-500={!isFollowing}
     class:border-gray-400={isFollowing}
-    class:border-blue-400={!isFollowing}
+    class:border-blue-500={!isFollowing}
 >
     {#if isFollowing}
     <span class="text-gray-600">Followed</span>
