5967d71208096145cfe0c7e584f0758bf35bee9e
boon4681 <boon4681dev@gmail.com>
2024-12-08 15:06:23 +0700

    update: move folder

--- src/lib/components/artcon/menu-button/menu-button.svelte
+++ src/lib/components/artcon/menu-button/menu-button.svelte
@@ -1,1 +1,6 @@
+<script lang="ts">
+    let className: string = "size-10";
+    export { className as class };
+</script>
+
 <div class="bg-white hover:bg-neutral-200 transition-all rounded p-1 text-sm px-2 cursor-pointer">My Profile</div>
--- src/lib/components/artcon/squarecard/squarecard.svelte
+++ src/lib/components/artcon/squarecard/squarecard.svelte
@@ -1,7 +1,7 @@
-<script>
+<script lang="ts">
     import HeartButton from "../heart-button/heart-button.svelte";
-    export let src;
-    export let alt;
+    export let src: string;
+    export let alt: string;
 </script>
 
 <div class="relative w-40 h-40 rounded-lg overflow-hidden shadow-lg select-none">
